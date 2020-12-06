ff689188c3b2b3c036d7ae40b6f0c9a89eb6fd23
mathxbs01 <75578668+mathxbs01@users.noreply.github.com>
2020-12-06 13:34:45 -0300

    Add files via upload

---
+++ Gauss Jacobi 2 ii.m
@@ -0,0 +1,40 @@
+############ENTRADA############
+
+
+A = [9 -2 3 2;2 8 -2 3;-3 2 11 -4;-2 3 2 10 ]
+
+B = [54.5;-14;12.5;-21]
+
+x0 = [0 ; 0; 0; 0]
+
+#########DADOS########
+
+KMAX = 100000;
+
+EPSILON = 0.05;
+
+#######INICIALIZAÇÃO#######
+
+K = 1;
+
+Dr = EPSILON + 1;
+
+D = diag([9 8 11 10])
+
+M = A - D;
+
+while(K=KMAX & Dr > EPSILON)
+
+####RESOLVA
+X = D\(B - M*x0);
+
+####CALCULE
+Dr = max(abs(X-x0))/max(abs(X));
+
+####ATUALIZE
+x0 = X;
+K = K+1;
+
+endwhile
+
+X
---
+++ Gauss Seidel - 2 i.m
@@ -0,0 +1,65 @@
+function [x,Dr]=MetodoGaussSeidel(A,b,x0,kmax,tau);
+
+#
+# Metodo de Gauss-Seidel para resolucao de um sistema linear Ax = b,
+# em que A eh uma matriz nao singular com diagonal nao-nula.
+#
+# Comando:
+# [x,Dr]=MetodoGaussSeidel(A,b,x0);
+#
+# em que as entradas sao: 
+#   A - matriz nxn. 
+#   b - termo do lado direito. 
+#   x0 - aproximacao inicial (default x0=zeros).
+#   kmax - numero maximo de iteracoes (default kmax=100).
+#   tau - tolerancia (default tau=1.e-6). 
+#
+# e as saidas sao:
+#   x - aproximacao para solucao.
+#   Dr - vetor contendo os erros relativos por iteracao.
+#
+
+# Definia x0 = [0;0;...;0] se nao for apresentada uma aproximacao inicial.
+
+
+A = [ 9 -2 3 2; 2 8 -2 3; -3 2 11 -4; -2 3 2 10];
+b = [ 54.5; -14; 12.5; -21];
+if nargin<3
+  x0 = zeros(size(b));
+end 
+
+# Definia kmax=100 se o numero maximo de iteracoes nao for definido.
+if nargin<4
+  kmax = 100;
+end 
+
+# Definia tau=1.e-6 se a tolerancia nao for definida.
+if nargin<5
+  tau = 1.e-6;
+end 
+
+# Defina as matrizes D e M;
+L = tril(A);
+U = A-L;
+
+# Inicialize;
+k = 1; 	# NÃ£o Ã© permitido Ã­ndice 0 para vetores! 
+Dr(k) = tau+1;
+while (k<=kmax)&(Dr(k)>tau)
+  interacao="interacao";
+  ktxt=mat2str(k);
+  puts(ktxt);
+  puts(" Interação ");
+  k = k+1;
+  x = L\(b-U*x0);
+   puts(" ");
+  puts(" Resultado: ")
+  disp(x);
+  Dr(k) = norm(x-x0,inf)/norm(x,inf);
+  x0 = x;
+  
+ 
+end
+
+# Excluir a primeira componente de Dr;
+Dr=Dr(2:end);
